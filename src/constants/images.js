7507051d9c4161d7b2d889a9d51f1647ff4483c3
senerwa <samuelsenerwa>
2023-06-13 12:20:09 +0300

    initial-commit

---
+++ src/constants/images.js
@@ -0,0 +1,31 @@
+import bg from '../assets/bg.png';
+import welcome from '../assets/image1.jpg';
+import overlaybg from '../assets/overlaybg.png';
+import service01 from '../assets/image2.jpg';
+import service02 from '../assets/image3.jpg';
+import service03 from '../assets/images4.jpg';
+import service04 from '../assets/images5.jpg';
+import service05 from '../assets/images6.jpg';
+import service06 from '../assets/images7.jpg';
+import logo1 from '../assets/logo1.png';
+import logo2 from '../assets/logo2.png';
+import logo3 from '../assets/logo3.png';
+import logo4 from '../assets/logo4.png';
+import logo5 from '../assets/logo5.png';
+
+export default {
+    bg,
+    welcome,
+    overlaybg,
+    service01,
+    service02,
+    service03,
+    service04,
+    service05,
+    service06,
+    logo1,
+    logo2,
+    logo3,
+    logo4,
+    logo5
+};
