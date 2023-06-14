07dfabccb6785699e4bd1e6feab3734314817a24
senerwa <samuelsenerwa>
2023-06-14 12:45:34 +0300

    media query

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { FaTools } from 'react-icons/fa';
 import images from '../../constants/images';
 
 import './Navbar.css';
 
 const  Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <nav className='app__navbar'>
         <div className='app__navbar-logo'>
@@ -18,13 +23,19 @@ const  Navbar = () => {
         <li className='p__opensans'><Link to="/contact">Contact</Link></li>
         </ul>
         <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color="#fff" fontsize={27} onClick={() => setToggleMenu(true)}/>
+        {toggleMenu && (
+        <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+          <FaTools fontsize={27} className="overlay__close" onClick={() => setToggleMenu(false)}/>
         <ul className='app__navbar-smallscreen-links'>
-            <li className='p__opensans'><a href='#home'>Home</a></li>
-            <li className='p__opensans'><a href='#about'>About</a></li>
-            <li className='p__opensans'><a href='#services'>Services</a></li>
-            <li className='p__opensans'><a href='#clients'>Clients</a></li>
-            <li className='p__opensans'><a href='#contact'>Contact</a></li>
+        <li className='p__opensans'><Link to="/">Home</Link></li>
+        <li className='p__opensans'><Link to="/about">About</Link></li>
+        <li className='p__opensans'><Link to="/services">Services</Link></li>
+        <li className='p__opensans'><Link to="/clients">Clients</Link></li>
+        <li className='p__opensans'><Link to="/contact">Contact</Link></li>
         </ul>
+        </div>
+        )}
         </div>
     </nav>
   )
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import { SubHeading } from "../../components";
 import { images } from "../../constants";
-import './Header.css'
+import './Header.css';
 
 const Header = () => (
-    <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__header app__wrapper section__padding" >
         <div className="app__wrapper_info">
             <div style={{marginBottom: '1rem'}}>
             <p className="p__cormorant" style={{color:'#000'}}>Who Is Hill Holdings Construction Limited?</p>
-        </div>
+            </div>
             <h1 className="app__header-h1"> Hill Holdings Construction Limited</h1>
             <p className="p__opensans" style={{ margin: '2rem 0', color: '#000' }} >Hill Holdings Construction Limited is a Kenyan Incorporated company and locally owned. The company offers Engineering Construction and Construction Project management. Hill Holdings Construction Limited is dedicated to organizations or Individuals seeking construction services.</p>
             <button type="button" className="custom__button">Explore Services</button>
@@ -17,7 +17,7 @@ const Header = () => (
         <div className="app__wrapper_img">
             <img src= {images.service01} alt="Welcome image"/>
             </div>
-    </div>  
+    </div> 
 );
 
 export default Header;
